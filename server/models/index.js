b934aeab1ba7ebd99b29ccc82da472adf06416eb
Framky007 <ajapsfranklin@gmail.com>
2017-07-19 18:00:40 +0100

    chore (maintain): Setup Sequelize
    Setup Sequelize as the Object relational mapper to be used in the API
    
    [Finishes #149092093]

---
+++ server/models/index.js
@@ -0,0 +1,38 @@
+'use strict';
+import config from __dirname + '/../config.json'[env];
+import fs from'fs';
+import path from'path';
+import Sequelize from'sequelize';
+
+
+const basename  = path.basename(module.filename);
+let db = {};
+const env = process.env.NODE_ENV || 'development';
+
+
+if (config.use_env_variable) {
+  const sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  const sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(function(file) {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
