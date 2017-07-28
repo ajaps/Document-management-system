56871cd3e95af2f5fa29acd32c7d13598e308bf9
Framky007 <ajapsfranklin@gmail.com>
2017-07-28 15:49:32 +0100

    chore (maintain): deply to Heroku
    Deploy app to Heroku
    
    [Finishes #148984085]

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,13 +5,9 @@ import path from 'path';
 // Load the gulp plugins into the `plugins` variable
 const plugins = loadPlugins();
 
-const paths = {
-  js: ['./**/*.js', '!dist/**', '!node_modules/**']
-};
-
 // Compile all Babel Javascript into ES5 and place in dist folder
 gulp.task('babel', () =>
-  gulp.src(paths.js, { base: '.' })
+  gulp.src(['server/**/*.js', './server.js'])
     .pipe(plugins.babel())
     .pipe(gulp.dest('dist'))
 );
